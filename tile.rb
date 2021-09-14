a24855cd789bbc1e7bd20e9beb84677254303229
Nicolas <nico-sv@hotmail.com>
2021-09-14 09:44:42 -0300

    filled the class Tile

---
+++ tile.rb
@@ -0,0 +1,24 @@
+class Tile
+    attr_reader :bomb, :revealed, :flagged
+    def initialize(pos,bomb=false)
+        @bomb = bomb
+        @revealed = false
+        @flagged = false
+        @pos = pos
+    end
+
+    def reveal!
+        @revealed = true
+    end
+
+    def flag!
+        @flagged = true
+    end
+
+    def neighbors
+        x, y = @pos
+        [[x-1,y],[x-1,y+1],[x,y+1],[x+1,y+1],[x+1,y],[x+1,y-1],[x,y-1],[x-1,y-1]]
+    end
+
+    
+end
