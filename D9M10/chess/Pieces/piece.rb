db09eeab32fe3db1f4ef0b4efe292c18fb38248f
Nicolas Saenz Valiente <nico-sv@hotmail.com>
2021-10-28 11:52:02 -0300

    refractored piece

--- D9M10/chess/Pieces/piece.rb
+++ D9M10/chess/Pieces/piece.rb
@@ -1,23 +1,29 @@
 require "colorize"
+
 class Piece
-    def initialize(color, board, pos)
-        @color = color
-        @board = board
-        @pos = pos
-        board.add_piece(self, @pos)
-    end
-    attr_reader :color, :board
-    attr_accessor :pos
+  def initialize(color, board, pos)
+    @color = color
+    @board = board
+    @pos = pos
+    board.add_piece(self, @pos)
+  end
+  attr_reader :color, :board
+  attr_accessor :pos
 
   def to_s #condition ? if_true : if_false
     " #{symbol} "
   end
 
-    def symbol
-        raise "symbol not defined"
-    end
+  def symbol
+    raise "symbol not defined"
+  end
 
-    def empty?
-        false
-    end
+  def empty?
+    false
+  end
+
+  def move_into_check?(end_pos)
+    dup = board.dup
+
+  end
 end
