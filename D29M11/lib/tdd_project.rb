6673efe1686b88514edcc3cd0066d544deaf4e7d
Nicolas Saenz Valiente <nico-sv@hotmail.com>
2021-11-29 10:47:33 -0300

    finished two_sum

--- D29M11/lib/tdd_project.rb
+++ D29M11/lib/tdd_project.rb
@@ -7,4 +7,15 @@ class Array
     arr
   end
 
+  def two_sum
+    arr = []
+    (0...self.length).each do |i1|
+      (i1+1...self.length).each do |i2|
+        if (self[i1] + self[i2]) == 0
+          arr << [i1,i2]
+        end
+      end
+    end
+    arr
+  end
 end
--- D29M11/spec/tdd_project_spec.rb
+++ D29M11/spec/tdd_project_spec.rb
@@ -9,3 +9,13 @@ describe "#my_uniq" do
     expect(arr.my_uniq).to eq ([1,2,8,4])
   end
 end
+
+describe "Array#two_sum" do
+  arr = [-1, 0, 2, -2, 1]
+  it "should return a 2D array" do
+    expect(arr.two_sum[0]).to be_a(Array)
+  end
+  it "should return the indeces in which the sum is 0" do
+    expect(arr.two_sum).to eq([[0, 4], [2, 3]])
+  end
+end
