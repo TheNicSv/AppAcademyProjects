02a10dd910ceaaf2d1e4a084f2ec51cb8e351fae
Nicolas Saenz Valiente <nico-sv@hotmail.com>
2021-11-29 10:25:41 -0300

    initial commit

---
+++ D29M11/spec/tdd_project_spec.rb
@@ -0,0 +1,11 @@
+describe "#my_uniq" do
+  arr = [1,2,8,1,4,2]
+  it "should take an array and return a new array" do
+    expect(arr.my_uniq).to_not be (arr)
+  end
+
+  it "should return an array with all the duped elements deleted and in the correct order" do
+    expect(arr.my_uniq).to eq ([1,2,8,4])
+  end
+
+end
